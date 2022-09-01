7a0b872a818fcfe6a2ef0d59a7ff111a0a8ac26d
Michael James Soria <michaeljamessoria@gmailcom>
2022-09-01 14:03:45 +0800

    first commit

---
+++ converter.py
@@ -0,0 +1,42 @@
+# import function2
+# print(function2.weight(67))
+
+# import random
+# for i in range (3):
+#     print(random.randint(10,30))
+
+# import random
+# member = ["michael","james","soria"]
+# leader = random.choice(member)
+# print(leader)
+
+# import random
+# class Dice:
+#     def roll(self):
+#         first = random.randint(1, 6)
+#         second = random.randint(1, 6)
+#         return first, second
+
+# dice = Dice()
+# print(dice.roll())
+
+
+import random
+first = random.randint(1,6)
+second = random.randint(1,6)
+print(first,second)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
